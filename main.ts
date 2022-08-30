aebc4595c401758a9af30504b63ddf2a7a6193a5
Roger O <43441351+RogerOM@users.noreply.github.com>
2022-08-31 00:28:54 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (luces) {
+        luces = 0
+        wuKong.setLightMode(wuKong.LightMode.OFF)
+    } else {
+        luces = 1
+        wuKong.setLightMode(wuKong.LightMode.BREATH)
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    wuKong.setAllMotor(21, -21)
+    basic.pause(2000)
+    wuKong.stopAllMotor()
+    basic.pause(500)
+    wuKong.setAllMotor(0, -31)
+    basic.pause(1000)
+    wuKong.setAllMotor(24, 0)
+    basic.pause(1000)
+    wuKong.setAllMotor(-23, 23)
+    basic.pause(1000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 2; index++) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+        basic.pause(1000)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 131)
+        basic.pause(1000)
+    }
+})
+let luces = 0
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.showRainbow(randint(1, 360), randint(1, 360))
